c712990ac37f923f4185ad93cd5c3cfa4c014931
VDSMath <matheusvdsdf@gmail.com>
2017-04-20 08:56:40 -0300

    Adicionada nave básica e terreno básico

---
+++ Assets/Code/Nave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour {
+
+	public float speed;
+	public Vector3 movement;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		movement = new Vector3 (0,0,0);
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move(){
+		movement = new Vector3 (speed * Input.GetAxis("Horizontal"), 0, 0);
+		rb.AddForce (movement);
+	}
+}
